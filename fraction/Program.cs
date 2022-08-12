322637f043f1fe2f6d26a1329e6cbc8d56d2692b
Akbar <akber.sharifov2004@gmail.com>
2022-08-12 17:54:55 +0300

    add fraction

---
+++ fraction/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static fraction solution_fraction(char solution, ref fraction A, fraction B) => solution switch
+        {
+            '*' => A *= B,
+            '/' => A /= B,
+            '+' => A += B,
+             _ => A -= B
+        };
+
+        public static void Main()
+        {
+            Console.Write("enter dividend 1 : ");
+            Int32 dividend1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter divider 1 : ");
+            Int32 divider1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter dividend 2 : ");
+            Int32 dividend2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter divider 2 : ");
+            Int32 divider2 = Convert.ToInt32(Console.ReadLine());
+
+            fraction A = new fraction(dividend: dividend1,divider: divider1), B = new fraction(dividend: dividend2, divider: divider2);
+
+            Console.Write("enter ( * / + - ) : ");
+            Char select = Convert.ToChar(Console.Read());
+
+            solution_fraction(solution: select, A: ref A, B: B);
+
+            Console.WriteLine(A);
+
+        }
+    }
+}
---
+++ fraction/fraction.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp4
+{
+    class fraction
+    {
+        public fraction() { }
+        public fraction(Int32 dividend , Int32 divider)
+        {
+            this.dividend = dividend * (divider >= 0 ? 1 : -1);
+            this.divider = divider * (divider >= 0 ? 1 : -1);
+        }
+
+        public static fraction operator +(fraction A, fraction B)
+        {
+            if (A.divider == B.divider) return new fraction(A.dividend + B.dividend, A.divider);
+            else if (A.divider % B.divider == 0 || B.divider % A.divider == 0)
+            {
+                return new fraction(A.dividend * (B.divider / A.divider == 0 ? 1 : B.divider / A.divider) +
+                                   B.dividend * (A.divider / B.divider == 0 ? 1 : A.divider / B.divider), (A.divider > B.divider ? A.divider : B.divider));
+            }
+            return new fraction(A.dividend * B.divider + B.dividend * A.divider, A.divider * B.divider);
+        }
+        public static fraction operator -(fraction A, fraction B)
+        {
+            if (A.divider == B.divider) return new fraction(A.dividend - B.dividend, A.divider);
+            else if (A.divider % B.divider == 0 || B.divider % A.divider == 0)
+            {
+                return new fraction(A.dividend * (B.divider / A.divider == 0 ? 1 : B.divider / A.divider) -
+                                   B.dividend * (A.divider / B.divider == 0 ? 1 : A.divider / B.divider), (A.divider > B.divider ? A.divider : B.divider));
+            }
+            return new fraction(A.dividend * B.divider - B.dividend * A.divider , A.divider * B.divider);
+        }
+        public static fraction operator *(fraction A, fraction B) => new fraction(A.dividend * B.dividend , A.divider * B.divider);
+        public static fraction operator /(fraction A, fraction B) => new fraction(A.dividend * B.divider , A.divider * B.dividend);
+        public static bool operator >(fraction A, fraction B) => ((Single)A.dividend / (Single)A.divider > B.dividend / (Single)B.divider);
+        public static bool operator <(fraction A, fraction B) => ((Single)A.dividend / (Single)A.divider < B.dividend / (Single)B.divider);
+        public static bool operator ==(fraction A, fraction B) => ((Single)A.dividend / (Single)A.divider == B.dividend / (Single)B.divider);
+        public static bool operator !=(fraction A, fraction B) => ((Single)A.dividend / (Single)A.divider != B.dividend / (Single)B.divider);
+
+        public override bool Equals(object? obj) => obj is fraction fraction &&
+                  (Single)this.dividend / (Single)this.divider == (Single)fraction.dividend / (Single)fraction.divider;
+        public override string ToString() => $"({this.dividend/this.divider}) {this.dividend%this.divider}/{this.divider}";
+        private Int32 dividend { get; } 
+        private Int32 divider { get; }
+
+    }
+}
